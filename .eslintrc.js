64353a0498ef64decdb7e071dc4f6741989a225e
sando <codemoochim@gmail.com>
2023-03-29 02:51:57 +0900

    Chore: tsconfig.json 파일 설정

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"airbnb",
+		"plugin:prettier/recommended",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "prettier"],
+	rules: {
+		"no-nested-ternary": 0,
+	},
+};
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const createError = require("http-errors");
+// const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
