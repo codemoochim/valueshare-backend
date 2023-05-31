af8629c161d6cfb9c219017399820abb9c60317b
sando <codemoochim@gmail.com>
2023-05-31 17:09:48 +0900

    Feat: sharp 라이브러리 사용하여 이미지 리사이징

--- src/controller/productCtrl.js
+++ src/controller/productCtrl.js
@@ -2,93 +2,84 @@ const productSrvc = require("../services/productSrvc");
 
 // 어드민 상품 목록 조회
 const getProductList = async (req, res, next) => {
-	try {
-		const productList = await productSrvc.findProductList();
-		res.json({ result: productList });
-	} catch (err) {
-		next(err);
-	}
+  try {
+    const productList = await productSrvc.findProductList();
+    res.json({ result: productList });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // 어드민 상품 추가
 const addProduct = async (req, res, next) => {
-	try {
-		const location = req.files;
-		const body = req.body;
-		const addedProduct = await productSrvc.createProduct(location, body);
-		res.json({ data: addedProduct });
-	} catch (err) {
-		next(err);
-	}
+  try {
+    const location = req.files[0].transforms;
+    const body = req.body;
+    const addedProduct = await productSrvc.createProduct(location, body);
+    res.json({ data: addedProduct });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // 어드민 상품 상세 수정
 const editProduct = async (req, res, next) => {
-	try {
-		const { productId } = req.params;
-		const body = req.body;
-		const location = req.files;
-		const editedProduct = await productSrvc.updateProduct(
-			productId,
-			body,
-			location,
-		);
-		res.json({ result: editedProduct });
-	} catch (err) {
-		next(err);
-	}
+  try {
+    const { productId } = req.params;
+    const body = req.body;
+    const location = req.files[0].transforms;
+    const editedProduct = await productSrvc.updateProduct(productId, body, location);
+    res.json({ result: editedProduct });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // 어드민 상품 삭제
 const removeProduct = async (req, res, next) => {
-	try {
-		const { productId } = req.params;
-		await productSrvc.deleteProduct(productId);
-		res.json({ message: "상품 삭제가 완료되었습니다." });
-	} catch (err) {
-		next(err);
-	}
+  try {
+    const { productId } = req.params;
+    await productSrvc.deleteProduct(productId);
+    res.json({ message: "상품 삭제가 완료되었습니다." });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // 유저, 어드민 상품 상세 조회
 const getProduct = async (req, res, next) => {
-	try {
-		const { productId } = req.params;
-		const foundProduct = await productSrvc.findProduct(productId);
-		res.json({ result: foundProduct });
-	} catch (err) {
-		next(err);
-	}
+  try {
+    const { productId } = req.params;
+    const foundProduct = await productSrvc.findProduct(productId);
+    res.json({ result: foundProduct });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // 유저 쿼리 필터링
 const getProductByQuery = async (req, res, next) => {
-	try {
-		const { categories } = req.query;
-		const brand = req.query.brand?.split(",");
-		// req.query.brand = req.query.brand?.split(",");
-		// const { brand } = req.query;
+  try {
+    const { categories } = req.query;
+    const brand = req.query.brand?.split(",");
+    // req.query.brand = req.query.brand?.split(",");
+    // const { brand } = req.query;
 
-		const page = Number(req.query.page || 1);
-		const perPage = Number(req.query.perPage || 20);
+    const page = Number(req.query.page || 1);
+    const perPage = Number(req.query.perPage || 20);
 
-		const foundProdcut = await productSrvc.findProductListByQuery(
-			categories,
-			brand,
-			page,
-			perPage,
-		);
-		res.json({ result: foundProdcut });
-	} catch (err) {
-		next(err);
-	}
+    const foundProdcut = await productSrvc.findProductListByQuery(categories, brand, page, perPage);
+    res.json({ result: foundProdcut });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
-	getProductList,
-	addProduct,
-	getProduct,
-	editProduct,
-	removeProduct,
-	getProductByQuery,
+  getProductList,
+  addProduct,
+  getProduct,
+  editProduct,
+  removeProduct,
+  getProductByQuery,
 };
--- src/middleware/imageUploadS3.js
+++ src/middleware/imageUploadS3.js
@@ -1,32 +1,39 @@
-const multer = require("multer");
-const { S3Client } = require("@aws-sdk/client-s3");
-const multerS3 = require("multer-s3");
-const { v4: uuid } = require("uuid");
 const dotenv = require("dotenv");
 dotenv.config();
-// aws s3 account
-const s3 = new S3Client({
-	credentials: {
-		accessKeyId: process.env.S3_ACCESS_KEY,
-		secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-	},
-	region: process.env.S3_BUCKET_REGION,
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const multerS3 = require("multer-s3-transform");
+const sharp = require("sharp");
+const { v4: uuid } = require("uuid");
+
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_BUCKET_REGION,
 });
+
 // multer storage
 const storage = multerS3({
-	s3,
-	bucket: process.env.S3_BUCKET_NAME,
-	key(req, file, cb) {
-		cb(null, `images/${uuid()}_${file.originalname}`);
-	},
-	// acl: "public-read",
-	// contentType: multerS3.AUTO_CONTENT_TYPE,
-	// ContentEncoding: "base64", // required
+  s3: new AWS.S3(),
+  bucket: process.env.S3_BUCKET_NAME,
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  shouldTransform: true,
+  transforms: [
+    {
+      id: "resized",
+      key: function (req, file, cb) {
+        cb(null, `images/${uuid()}_${file.originalname}`);
+      },
+      transform: function (req, file, cb) {
+        cb(null, sharp().resize(1000, 1000).jpeg());
+      },
+    },
+  ],
 });
 // imgUpload
 const imageUploadS3 = multer({
-	storage,
-	limits: { fileSize: 5 * 1024 * 1024 },
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 module.exports = imageUploadS3;
